5038614627c4ff345a548cbd0ecba12826f5f286
lyg0114 <whdnseowkd@naver.com>
2024-06-03 16:22:12 +0200

    feat : BaseEntity 추가

--- src/main/java/com/install/InstallApplication.java
+++ src/main/java/com/install/InstallApplication.java
@@ -1,13 +1,30 @@
 package com.install;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class InstallApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(InstallApplication.class, args);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+    return new JPAQueryFactory(em);
   }
 
 }
