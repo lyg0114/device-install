c6f34c1f61f0f9d4080fd8f6f666737c523147fe
lyg0114 <whdnseowkd@naver.com>
2024-06-03 14:45:17 +0200

    init

---
+++ src/main/java/com/install/InstallApplication.java
@@ -0,0 +1,13 @@
+package com.install;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstallApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InstallApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/install/InstallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.install;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstallApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
