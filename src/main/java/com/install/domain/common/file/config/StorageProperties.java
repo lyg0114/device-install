9f9c09a81ccd618db807e2949c396f99eb187dad
lyg0114 <whdnseowkd@naver.com>
2024-06-07 10:42:32 +0200

    chor : 파일관련 설정파일 추가

---
+++ src/main/java/com/install/domain/common/file/config/StorageProperties.java
@@ -0,0 +1,17 @@
+package com.install.domain.common.file.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+  private String location;
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+}
