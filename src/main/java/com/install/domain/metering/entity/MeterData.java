41c2d0b966df86d79ad1a2d84a18b35ad3f9ed02
lyg0114 <whdnseowkd@naver.com>
2024-06-11 20:06:08 +0200

    feat : entity 생성

--- src/main/java/com/install/domain/metering/entity/MeterData.java
+++ src/main/java/com/install/domain/metering/entity/MeterData.java
@@ -3,11 +3,16 @@ package com.install.domain.metering.entity;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
+import com.install.domain.common.BaseTimeEntity;
+import com.install.domain.modem.entity.Modem;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,11 +29,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PROTECTED)
 @Table(name = "meter_data")
 @Entity
-public class MeterData {
+public class MeterData extends BaseTimeEntity {
 
   @Id
   @GeneratedValue(strategy = IDENTITY)
   @Column(name = "meter_data_id")
   private Long id;
 
+  @ManyToOne
+  @JoinColumn(name = "modem_id")
+  private Modem modem;
+
+  @Column(name = "metering_value", nullable = false, precision = 10, scale = 3)
+  private BigDecimal meteringValue;
+
+  @Column(name = "metering_temp", nullable = false, precision = 10, scale = 3)
+  private BigDecimal meteringTemp;
+
+  @Column(name = "metering_state", nullable = false)
+  private String meteringState;
 }
--- src/main/java/com/install/domain/metering/entity/MeterInfo.java
+++ src/main/java/com/install/domain/metering/entity/MeterInfo.java
@@ -3,11 +3,16 @@ package com.install.domain.metering.entity;
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
+import com.install.domain.common.BaseTimeEntity;
+import com.install.domain.modem.entity.Modem;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,10 +29,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = PROTECTED)
 @Table(name = "meter_info")
 @Entity
-public class MeterInfo {
+public class MeterInfo extends BaseTimeEntity {
 
   @Id
   @GeneratedValue(strategy = IDENTITY)
   @Column(name = "meter_info_id")
   private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "modem_id")
+  private Modem modem;
+
+  @Column(name = "metering_usage", nullable = false, precision = 10, scale = 3)
+  private BigDecimal meteringUsage;
+
+  @Column(name = "metering_temp", nullable = false, precision = 10, scale = 3)
+  private BigDecimal meteringTemp;
+
+  @Column(name = "metering_state", nullable = false)
+  private String meteringState;
 }
