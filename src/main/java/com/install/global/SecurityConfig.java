0a4190ed59e90143e418aeafaeb096f670a9f99d
lyg0114 <whdnseowkd@naver.com>
2024-06-12 10:39:11 +0200

    refactor : security Deprecated 기능 제거

--- src/main/java/com/install/global/SecurityConfig.java
+++ src/main/java/com/install/global/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -27,7 +26,6 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 @Slf4j
 @RequiredArgsConstructor
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 public class SecurityConfig {
 
