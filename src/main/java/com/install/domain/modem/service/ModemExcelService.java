c75e0b39b0462fb49e7a7dfa0821d758eaa7689c
lyg0114 <whdnseowkd@naver.com>
2024-06-13 11:27:12 +0200

    feat : 엑셀로부터 데이터 읽어오기

--- src/main/java/com/install/domain/modem/service/ModemExcelService.java
+++ src/main/java/com/install/domain/modem/service/ModemExcelService.java
@@ -1,5 +1,9 @@
 package com.install.domain.modem.service;
 
+import static com.install.domain.code.entity.CodeSet.*;
+import static java.lang.String.valueOf;
+import static org.apache.poi.ss.usermodel.DateUtil.*;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -7,7 +11,6 @@ import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.install.domain.modem.dto.ModemDto.ModemRequest;
 import com.install.domain.modem.entity.repository.ModemRepository;
 import com.install.global.websocket.handler.ProgressWebSocketHandler;
 
@@ -35,63 +39,64 @@ public class ModemExcelService {
 	private final ModemRepository modemRepository;
 	private final ProgressWebSocketHandler progressWebSocketHandler;
 
-	private static void sleep() {
-		try {
-			Thread.sleep(50);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	public List<List<String>> readExcelFile(MultipartFile file) throws IOException {
-		List<List<String>> data = new ArrayList<>();
-		try (InputStream is = file.getInputStream(); Workbook workbook = new HSSFWorkbook(is)) {
-			Sheet sheet = workbook.getSheetAt(0);
-
-			for (Row row : sheet) {
-				List<String> rowData = new ArrayList<>();
-
-				for (Cell cell : row) {
-					switch (cell.getCellType()) {
-						case STRING -> rowData.add(cell.getStringCellValue());
-						case NUMERIC -> {
-							if (DateUtil.isCellDateFormatted(cell)) {
-								rowData.add(cell.getDateCellValue().toString());
-							} else {
-								rowData.add(String.valueOf(cell.getNumericCellValue()));
-							}
-						}
-						case BOOLEAN -> rowData.add(String.valueOf(cell.getBooleanCellValue()));
-						case FORMULA -> rowData.add(cell.getCellFormula());
-						default -> rowData.add("");
-					}
-				}
-
-				data.add(rowData);
-			}
-		}
-
-		return data;
-	}
-
 	public void uploadModemExcel(MultipartFile file, String sessionId) {
+		List<ModemRequest> requests = readExcelFile(file);
+		int totalRows = requests.size();
 
-		int totalRows = getTotalRows(file);
 		try {
 			for (int i = 0; i < totalRows; i++) {
+				// upload logic
 				int progress = (i + 1) * 100 / totalRows;
 				progressWebSocketHandler.sendProgressUpdate(sessionId, Integer.toString(progress));
-				sleep();
 			}
 
 			progressWebSocketHandler.closeSession(sessionId);
+
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-
 	}
 
-	private int getTotalRows(MultipartFile file) {
-		return 100;
+	public List<ModemRequest> readExcelFile(MultipartFile file) {
+		List<ModemRequest> requests = new ArrayList<>();
+		try (InputStream is = file.getInputStream(); Workbook workbook = new HSSFWorkbook(is)) {
+			Sheet sheet = workbook.getSheetAt(0);
+
+			boolean firstRow = true;
+
+			for (Row row : sheet) {
+				// 첫 번째 행 skip
+				if (firstRow) {
+					firstRow = false;
+					continue;
+				}
+
+				requests.add(ModemRequest.builder()
+					.modemNo(extractedData(row.getCell(0)))
+					.imei(extractedData(row.getCell(1)))
+					.buildCompany(extractedData(row.getCell(2)))
+					.modemTypeCd(MODEM_TYPE_NBIOT.getCode())
+					.modemStatusCd(MODEM_STAUTS_NORMAL.getCode())
+					.build());
+			}
+		} catch (IOException e) {
+			// TODO : 적절한 예외처리 로직 필요
+			throw new RuntimeException(e);
+		}
+
+		return requests;
 	}
+
+	private String extractedData(Cell cell) {
+		String value;
+		switch (cell.getCellType()) {
+			case STRING -> value = cell.getStringCellValue();
+			case NUMERIC -> value = isCellDateFormatted(cell) ? cell.getDateCellValue().toString() : valueOf(cell.getNumericCellValue());
+			case BOOLEAN -> value = valueOf(cell.getBooleanCellValue());
+			case FORMULA -> value = cell.getCellFormula();
+			default -> value = "";
+		}
+		return value;
+	}
+
 }
