26b5681e76b9d4b7d03ec79b870f5cefa7829334
lyg0114 <whdnseowkd@naver.com>
2024-06-21 22:07:53 +0200

    feat : 공통 Bean 설정정보 분리

--- src/main/java/com/install/InstallApplication.java
+++ src/main/java/com/install/InstallApplication.java
@@ -1,23 +1,11 @@
 package com.install;
 
-import static com.install.domain.code.entity.CodeSet.*;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.install.domain.code.entity.repository.CodeRepository;
 import com.install.domain.common.file.config.StorageProperties;
-import com.install.domain.common.file.service.StorageService;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
-import jakarta.persistence.EntityManager;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -26,30 +14,5 @@ public class InstallApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InstallApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-		return new JPAQueryFactory(em);
-	}
-
-	@Bean
-	public CommandLineRunner fileInit(StorageService storageService) {
-		return args -> {
-			storageService.init();
-		};
-	}
-
-	@Profile("local")
-	@Bean
-	public CommandLineRunner codeInit(CodeRepository codeRepository) {
-		return args -> {
-			codeRepository.saveAll(getAllCodes());
-		};
 	}
 }
